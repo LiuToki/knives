1eb2851662adaa8fa114cbf53ec2cd3a86a8e884
LiuToki <ray.usaneko@gmail.com>
2024-02-17 15:36:36 +0900

    [tcp-echo-server] 文字コードとインデントを修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 #ifdef _MSC_VER
 #pragma warning(push)
@@ -11,67 +11,67 @@
 #endif
 
 class Server {
-    boost::asio::io_service& m_ioService;
-    boost::asio::ip::tcp::acceptor m_acceptor;
-    boost::asio::ip::tcp::socket m_socket;
-    boost::asio::streambuf m_rcvBuf;
+	boost::asio::io_service& m_ioService;
+	boost::asio::ip::tcp::acceptor m_acceptor;
+	boost::asio::ip::tcp::socket m_socket;
+	boost::asio::streambuf m_rcvBuf;
 
 public:
-    Server(boost::asio::io_service& io_service)
-        : m_ioService(io_service),
-          m_acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 31400)),
-          m_socket(io_service) {}
+	Server(boost::asio::io_service& io_service)
+		: m_ioService(io_service),
+		  m_acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 31400)),
+		  m_socket(io_service) {}
 
-    void start()
-    {
-        StartAccept();
-    }
+	void start()
+	{
+		StartAccept();
+	}
 
 private:
-    // 接続待機.
-    void StartAccept()
+	// 接続待機.
+	void StartAccept()
 	{
-        m_acceptor.async_accept(
-            m_socket,
-            boost::bind(&Server::OnAccept, this, boost::asio::placeholders::error));
-    }
+		m_acceptor.async_accept(
+			m_socket,
+			boost::bind(&Server::OnAccept, this, boost::asio::placeholders::error));
+	}
 	
 	// 接続待機完了.
-void OnAccept(const boost::system::error_code& error)
-    {
-        if (error) {
-            std::cout << "accept failed: " << error.message() << std::endl;
-            return;
-        }
+	void OnAccept(const boost::system::error_code& error)
+	{
+		if (error) {
+			std::cout << "accept failed: " << error.message() << std::endl;
+			return;
+		}
 
-        start_receive();
-    }
+		start_receive();
+	}
 
-    // メッセージ受信
-    void start_receive()
-    {
-        boost::asio::async_read(
-            m_socket,
-            m_rcvBuf,
-            boost::asio::transfer_all(),
-            boost::bind(&Server::on_receive, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
-    }
+	// メッセージ受信
+	void start_receive()
+	{
+		boost::asio::async_read(
+			m_socket,
+			m_rcvBuf,
+			boost::asio::transfer_all(),
+			boost::bind(&Server::on_receive, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+	}
 
-    // 受信完了
-    // error : エラー情報
-    // bytes_transferred : 受信したバイト数
-    void on_receive(const boost::system::error_code& error, size_t /*bytes_transferred*/)
-    {
-        if (error && error != boost::asio::error::eof) {
-            std::cout << "receive failed: " << error.message() << std::endl;
-        }
-        else {
-            const char* data = boost::asio::buffer_cast<const char*>(m_rcvBuf.data());
-            std::cout << data << std::endl;
+	// 受信完了
+	// error : エラー情報
+	// bytes_transferred : 受信したバイト数
+	void on_receive(const boost::system::error_code& error, size_t /*bytes_transferred*/)
+	{
+		if (error && error != boost::asio::error::eof) {
+			std::cout << "receive failed: " << error.message() << std::endl;
+		}
+		else {
+			const char* data = boost::asio::buffer_cast<const char*>(m_rcvBuf.data());
+			std::cout << data << std::endl;
 
-            m_rcvBuf.consume(m_rcvBuf.size());
-        }
-    }
+			m_rcvBuf.consume(m_rcvBuf.size());
+		}
+	}
 };
 
 int main()
